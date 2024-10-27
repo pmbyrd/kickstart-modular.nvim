86e8a96a5c2201848381954a25b773800502d128
Perriesha Byrd <pmbyrd@gmail.com>
2024-10-26 20:08:53 -0700

    Additionaly tweaking

--- lua/custom/plugins/html.lua
+++
@@ -1,4 +0,0 @@
--- write a function that prevents this file from throwing an error if the it is empty
-if true then
-  print 'hello'
-end
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,8 +6,8 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
@@ -66,6 +66,6 @@ keymap('t', '<C-k>', '<C-\\><C-n><C-w>k')
 keymap('t', '<C-l>', '<C-\\><C-n><C-w>l')
 
 -- Better navigation
-keymap('n', ';', ':')
+-- keymap('n', ';', ':')
 -- vim: ts=2 sts=2 sw=2 et
 -- vim: ts=2 sts=2 sw=2 et
