6128966b3e622b838fc47492548950d4b5a100bc
Perriesha Byrd <pmbyrd@gmail.com>
2024-10-26 20:11:08 -0700

    Added aliases that I like on my system

---
+++ aliases.sh
@@ -0,0 +1,75 @@
+#Aliases
+
+#Navigation
+alias dwn='cd ~/Downloads'
+#alias spring='cd ~/Springboard/'
+alias repos='cd ~/Repos/'
+alias prac='cd ~/Practice/'
+alias proj='cd ~/Projects/'
+alias docs='cd ~/Documents/'
+#alias obs='nvim ~/Documents/Obsidian/'
+alias conf='cd ~/.config/'
+alias als='nvim ~/.config/aliases.sh'
+alias soals='source ~/.config/aliases.sh'
+alias nvimconf='cd ~/.config/nvim/'
+alias nvcfg='nvim ~/.config/nvim/init.vim'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ~='cd ~'
+alias trek='cd ~/Projects/trektrek/'
+#Files
+alias ll='ls -la'
+alias l='ls -CF'
+
+#Operations
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+alias mkdir='mkdir -p'
+alias rd='rmdir'
+alias rmd='rm -ri'
+
+#Git
+alias gs='git status'
+alias ga='git add'
+alias gcm='git commit -m'
+alias gb='git branch'
+alias gl='git log --oneline --decorate --graph'
+alias gd='git diff'
+alias gps='git push'
+alias gpl='gut pull'
+alias gcl='git clone'
+# Package manager for Ubuntu based systems.
+alias update='sudo apt update && sudo apt upgrade'
+alias install='sudo apt install'
+alias remove='sudo apt remove'
+
+# Process management
+alias ps='ps aux'
+alias top='htop'
+alias kill='kill -9'
+
+# System information
+alias df='df -h'
+alias du='du -h'
+alias free='free -h'
+alias ifconfig='ip -c a'
+
+#Flatpack
+alias wezterm='flatpak run org.wezfurlong.wezterm'
+alias py='python3'
+
+#Commands
+alias c='clear'
+alias h='history'
+alias p='python3'
+alias v.='nvim .'
+alias v='nvim'
+alias sv='sudo nvim'
+
+alias spv='source venv/bin/activate'
+alias dv='deactivate'
+alias mypass='ls ~/etc/passwd'
+
+#Software
+alias obs='flatpak run md.obsidian.Obsidian'
